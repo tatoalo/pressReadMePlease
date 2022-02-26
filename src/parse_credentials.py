c0fce5e1e98abbedbd9a246e88c7e4df07a100c6
tatoalo <apogliaghi@gmail.com>
2022-02-26 14:52:28 +0100

    feat: added type annotation support for parse_credentials.py

--- src/parse_credentials.py
+++ src/parse_credentials.py
@@ -1,11 +1,12 @@
 import sys
 
 from notify import Notifier
+from typing import Optional
 
 NOTIFY = Notifier()
 
 
-def extract_keys(path="auth_data.txt", notification_service=None):
+def extract_keys(path: str = "auth_data.txt", notification_service: Optional = None):
     """
     The file must be in the same `src` working directory
     of the other python files, the following must be the format (each line with an information/credential):
@@ -27,7 +28,7 @@ def extract_keys(path="auth_data.txt", notification_service=None):
         :param path:                                path to authorization file
         :param notification_service:                Notifier object
 
-        :return str tuple, str tuple, str tuple     mlol link, mlol credentials, pressreader credentials
+        :return str tuple, List[str] tuple, str tuple     mlol link, mlol credentials, pressreader credentials
       """
 
     if notification_service is not None:
