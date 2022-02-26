ae9d66430045894cd007a2d3a0496d868270aa50
tatoalo <apogliaghi@gmail.com>
2022-02-26 19:17:43 +0100

    feat: working towards playwright support for pressreader.py

--- src/pressreader.py
+++ src/pressreader.py
@@ -3,80 +3,52 @@ import time
 
 from notify import Notifier
 
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+from playwright.sync_api import Browser, Page
+from playwright.sync_api import TimeoutError
 
 NOTIFY = Notifier()
 
 
-def visit_pressreader(b, pressreader_auth="", notification_service=None):
+def visit_pressreader(b: Browser, page: Page, pressreader_auth: str = "", notification_service: Notifier = None):
     if notification_service is not None:
         global NOTIFY
         NOTIFY = notification_service
 
-    # Switching to pressreader tab, target was `_blank' so I need
-    # to iterate over the available handles, no biggie.
-    # Being the target `_blank` the focus tab is immediately pressreader,
-    # switch is required for changing page objects reference
-    tabs = b.window_handles
-    b.switch_to.window(tabs[1])
     print("Visiting Pressreader...")
 
     try:
-        publications_button = WebDriverWait(b, 5).until(
-            EC.presence_of_element_located(
-                (By.XPATH, "//label[@data-bind='click: selectTitle']/button[@type='submit']"))
-        )
+        sign_in_button = page.query_selector(".btn-login")
+        sign_in_button.click()
 
-        b.execute_script("arguments[0].click();", publications_button)
+        login_pressreader(b, page, pressreader_auth)
 
-        login_pressreader(b, pressreader_auth)
-
-        # Waiting for 3 second before ceasing operations
-        time.sleep(3)
-
-    except TimeoutException:
-        login_pressreader(b, pressreader_auth)
-    except NoSuchElementException:
-        try:
-            time.sleep(2)
-            publications_button = b.find_element_by_xpath(
-                "//label[@data-bind='click: selectTitle']/button[@type='submit']")
-            b.execute_script("arguments[0].click();", publications_button)
-        except Exception as e:
-            if not NOTIFY.disabled:
-                NOTIFY.send_message(f"Error in {visit_pressreader.__name__} ; {e}")
-                NOTIFY.screenshot_client.take_screenshot('error')
-                NOTIFY.screenshot_client.remove_screenshot()
-            b.close()
-            sys.exit(f"Element not found! {visit_pressreader.__name__} ; {e}")
+    except Exception as e:
+        if not NOTIFY.disabled:
+            NOTIFY.send_message(f"Error in {visit_pressreader.__name__} ; {e}")
+            NOTIFY.screenshot_client.take_screenshot('error')
+            NOTIFY.screenshot_client.remove_screenshot()
+        b.close()
+        sys.exit(f"Element not found! {visit_pressreader.__name__} ; {e}")
 
 
-def login_pressreader(b, pressreader_auth):
+def login_pressreader(b: Browser, page: Page, pressreader_auth: str):
     try:
         print("Logging into Pressreader...")
-        time.sleep(5)
         username, password = pressreader_auth[0], pressreader_auth[1]
 
-        login_icon = locate_login_button(b)
-        if login_icon:
-            b.execute_script("arguments[0].click();", login_icon)
-        else:
-            b.close()
-            NOTIFY.send_message(f"Error in {login_pressreader.__name__} ; Login icon not found!")
-            sys.exit(f"Element not found! {login_pressreader.__name__} ; Login icon not found!")
+        page.fill("input[type='email']", "lol", timeout=0)
+        page.fill("input[type='password']", "lol", timeout=0)
+        breakpoint()
 
-        time.sleep(2)
-        pressreader_id = b.find_element_by_xpath("//input[@type='email']")
-        pressreader_psw = b.find_element_by_xpath("//input[@type='password']")
-        pressreader_id.send_keys(username)
-        pressreader_psw.send_keys(password)
+        # pressreader_id = b.find_element_by_xpath("//input[@type='email']")
+        # pressreader_psw = b.find_element_by_xpath("//input[@type='password']")
+        # pressreader_id.send_keys(username)
+        # pressreader_psw.send_keys(password)
 
         # Unchecking `stay signed in checkbox`
-        stay_signed_in_checkbox = b.find_element_by_xpath("//input[@type='checkbox']")
-        b.execute_script("arguments[0].click();", stay_signed_in_checkbox)
+        stay_signed_in_checkbox = page.locator("label:has-text(\"Stay signed in\")")
+        if not stay_signed_in_checkbox.is_checked():
+            stay_signed_in_checkbox.click()
 
         # Sign in procedure
         submit_button = b.find_element_by_xpath("//div[@class='pop-group']/a[@role='link']")
@@ -92,25 +64,6 @@ def login_pressreader(b, pressreader_auth):
             NOTIFY.screenshot_client.remove_screenshot()
         b.close()
         sys.exit(f"Element not found! {login_pressreader.__name__} ; {e}")
-
-
-# Moved logic to specific method in order to better handle the two existing cases found so far
-def locate_login_button(b):
-    login_icon = None
-
-    try:
-        login_icon = b.find_element_by_xpath("//button[@class='btn btn-login']")
-    except NoSuchElementException:
-        try:
-            login_icon = b.find_element_by_xpath("//button[@class='btn btn-account']")
-        except StaleElementReferenceException:
-            # DOM has been refreshed, let's force find it in order not to lose
-            # the reference pointer
-            print("DOM has been refreshed, let's retry...")
-            time.sleep(2)
-            login_icon = b.find_element_by_xpath("//button[@class='btn btn-login']")
-
-    return login_icon
 
 
 def failed_login_procedure(b):
