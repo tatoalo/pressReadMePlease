756b467bd2872b10d291b6f62828a81fef30f8cb
tatoalo <apogliaghi@gmail.com>
2022-02-28 21:06:21 +0100

    feat: Concluded playwright support for pressreader.py

--- src/pressreader.py
+++ src/pressreader.py
@@ -1,10 +1,9 @@
 import sys
-import time
-
-from notify import Notifier
 
 from playwright.sync_api import Browser, Page
 from playwright.sync_api import TimeoutError
+
+from notify import Notifier
 
 NOTIFY = Notifier()
 
@@ -21,6 +20,7 @@ def visit_pressreader(b: Browser, page: Page, pressreader_auth: str = "", notifi
         sign_in_button.click()
 
         login_pressreader(b, page, pressreader_auth)
+        logout_pressreader(page)
 
     except Exception as e:
         if not NOTIFY.disabled:
@@ -36,9 +36,8 @@ def login_pressreader(b: Browser, page: Page, pressreader_auth: str):
         print("Logging into Pressreader...")
         username, password = pressreader_auth[0], pressreader_auth[1]
 
-        page.fill("input[type='email']", "lol@test.com", timeout=0)
-        page.fill("input[type='password']", "lol", timeout=0)
-        breakpoint()
+        page.fill("input[type='email']", username, timeout=0)
+        page.fill("input[type='password']", password, timeout=0)
 
         # Unchecking `stay signed in checkbox`
         stay_signed_in_checkbox = page.locator("label:has-text(\"Stay signed in\")")
@@ -46,16 +45,12 @@ def login_pressreader(b: Browser, page: Page, pressreader_auth: str):
             stay_signed_in_checkbox.click()
 
         # Sign in procedure
-        submit_button = page.query_selector(".btn-action")
-
-        with page.expect_response("**/services/auth/**") as response_info:
-            submit_button.click()
-
-        response = response_info.value
-        print(f"{response.status}, {response.status_text}")
-
+        submit_button = page.query_selector(".btn-action >> text=Sign In")
+        submit_button.click()
         # Checking whether credentials were wrong
-        failed_login_procedure(b)
+        failed_login_procedure(b, page)
+
+        select_publication_button(page)
 
     except Exception as e:
         if not NOTIFY.disabled:
@@ -68,11 +63,10 @@ def login_pressreader(b: Browser, page: Page, pressreader_auth: str):
 
 def failed_login_procedure(b):
     try:
-        time.sleep(2)
-        wrong_credentials_warning = b.find_element_by_xpath("//div[@class='infomsg']/p").text
-        if 'invalid' in wrong_credentials_warning.lower():
+        wrong_credentials_warning = p.query_selector(".infomsg >> text=Invalid")
+        if wrong_credentials_warning.is_visible():
             b.close()
             NOTIFY.send_message("Wrong Pressreader credentials!")
             sys.exit("Login failed, please check your Pressreader credentials!")
-    except NoSuchElementException:
+    except TimeoutError:
         pass
