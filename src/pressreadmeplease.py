fbec69fd7dff3c174ecb95631cbd5d9f405f9810
tatoalo <apogliaghi@gmail.com>
2022-03-01 19:00:41 +0100

    fix: Added context support in pressreadmeplease.py
    It will also be instrumental in the new Screenshot support

--- src/pressreadmeplease.py
+++ src/pressreadmeplease.py
@@ -8,7 +8,7 @@ from pressreader import visit_pressreader
 
 from dotenv import load_dotenv
 from pathlib import Path
-from playwright.sync_api import sync_playwright, Browser
+from playwright.sync_api import sync_playwright, Browser, Page, BrowserContext
 
 
 PROJECT_ROOT = Path(__file__).parent
@@ -28,29 +28,36 @@ else:
 def init_chromium():
     print("Launching Chrome...")
 
-    p = sync_playwright().start()
-    b = p.chromium.launch(
-        headless=False,
-        traces_dir=PROJECT_ROOT
+    playwright = sync_playwright().start()
+    b = playwright.chromium.launch(
+        headless=False
     )
-    page = b.new_page()
-    config_page(page, timeout=10000)
 
     if NOTIFY.disabled is False:
         NOTIFY.screenshot_client.browser = b
 
-    return b, page
+    return b
 
 
-def config_page(page: Page, timeout: int = 0, window_size=None):
+def create_context(b: Browser):
+    context = b.new_context()
+    context.set_default_timeout(10000)
+    context.tracing.start(screenshots=True, snapshots=True)
+    page = context.new_page()
+    config_page(page)
+
+    return context
+
+
+def config_page(page: Page, window_size=None):
     if window_size is None or not window_size.get('width') or not window_size.get('height'):
         window_size = {"width": 1920, "height": 1080}
-    page.set_default_timeout(timeout)
     page.set_viewport_size(viewport_size=window_size)
 
 
-def close_browser(b: Browser):
+def close_browser(b: Browser, c: BrowserContext):
     print("Terminating Chrome...")
+    c.tracing.stop(path=PROJECT_ROOT / "trace.zip")
     b.close()
 
 
@@ -58,17 +65,19 @@ def main():
     # Retrieve credentials and MLOL entrypoint
     mlol_link, mlol_credentials, pressreader_credentials = extract_keys(path=PROJECT_ROOT / "auth_data.txt", notification_service=NOTIFY)
 
-    b, page = init_chromium()
+    b = init_chromium()
+    context = create_context(b)
+
     pressreader_tab = visit_MLOL(
         b,
-        page,
+        context.pages[0],
         mlol_entrypoint=mlol_link,
         mlol_auth=mlol_credentials,
         notification_service=NOTIFY
     )
     visit_pressreader(b, page=pressreader_tab, pressreader_auth=pressreader_credentials, notification_service=NOTIFY)
     print("*** Automation flow has terminated correctly ***")
-    close_browser(b)
+    close_browser(b, context)
 
 
 if __name__ == "__main__":
