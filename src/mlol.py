a40b74e8381eddfcbbc3f22319768d53444bc72c
tatoalo <apogliaghi@gmail.com>
2022-03-09 18:35:47 +0100

    fix: Fixed some methods signatures after chromium support

--- src/mlol.py
+++ src/mlol.py
@@ -1,30 +1,35 @@
 import sys
-
-from notify import Notifier
 from typing import List
-from playwright.sync_api import Browser, Page
+
+from playwright.sync_api import Page
 from playwright.sync_api import TimeoutError
 
+from chromium import Chromium
+from notify import Notifier
 
 NOTIFY = Notifier()
+chromium = None
 
 
-def visit_MLOL(b: Browser, page: Page, mlol_entrypoint: str = "", mlol_auth: List[str] = [], notification_service: Notifier = None):
+def visit_MLOL(mlol_entrypoint: str = "", mlol_auth: List[str] = [], notification_service: Notifier = None):
+    global chromium
+    chromium = Chromium.get_chromium()
     if notification_service is not None:
         global NOTIFY
         NOTIFY = notification_service
 
     print("Visiting MLOL...")
-    page.goto(mlol_entrypoint, timeout=0)
+    page = chromium.context.pages[0]
+    chromium.visit_site(page, mlol_entrypoint)
 
-    perform_login(b, page, mlol_auth)
+    perform_login(page, mlol_auth)
     verify_modal_presence(page)
     new_page_tab = navigate_to_newspapers(b, page)
 
     return new_page_tab
 
 
-def perform_login(b: Browser, page: Page, mlol_auth: List[str]):
+def perform_login(page: Page, mlol_auth: List[str]):
     print("Logging into MLOL...")
     username, password = mlol_auth[0], mlol_auth[1]
 
@@ -35,22 +40,22 @@ def perform_login(b: Browser, page: Page, mlol_auth: List[str]):
         page.click("input[type='submit']", timeout=0)
 
         # Checking failed login procedure
-        failed_login_procedure(b, page)
+        failed_login_procedure(page)
 
     except Exception as e:
         if not NOTIFY.disabled:
             NOTIFY.send_message(f"Error in {perform_login.__name__} ; {e}")
-            NOTIFY.screenshot_client.take_screenshot('error')
+            NOTIFY.screenshot_client.take_screenshot(page, 'error')
             NOTIFY.screenshot_client.remove_screenshot()
-        b.close()
+        chromium.clean()
         sys.exit(f"Element not found! {perform_login.__name__} ; {e}")
 
 
-def failed_login_procedure(b: Browser, page: Page):
+def failed_login_procedure(page: Page):
     try:
         warning_failed_login = page.text_content(".page-title").lower()
         if 'avviso' in warning_failed_login:
-            b.close()
+            chromium.clean()
             if not NOTIFY.disabled:
                 NOTIFY.send_message("Wrong MLOL credentials!")
             sys.exit("Login failed, please check your MLOL credentials!")
@@ -58,7 +63,7 @@ def failed_login_procedure(b: Browser, page: Page):
         pass
 
 
-def navigate_to_newspapers(b: Browser, page: Page):
+def navigate_to_newspapers(page: Page):
     try:
         # Clicking on catalogue
         typologies_menu_entry = page.query_selector("#caricatip")
@@ -85,9 +90,9 @@ def navigate_to_newspapers(b: Browser, page: Page):
     except Exception as e:
         if not NOTIFY.disabled:
             NOTIFY.send_message(f"Error in {navigate_to_newspapers.__name__} ; {e}")
-            NOTIFY.screenshot_client.take_screenshot('error')
+            NOTIFY.screenshot_client.take_screenshot(page, 'error')
             NOTIFY.screenshot_client.remove_screenshot()
-        b.close()
+        chromium.clean()
         sys.exit(f"Error in {navigate_to_newspapers.__name__} ; {e}")
 
 
@@ -95,9 +100,10 @@ def verify_modal_presence(page: Page):
     try:
         page.wait_for_selector("#FavModal")
         print("Modal found on MLOL entry")
-        NOTIFY.send_message("Modal found in MLOL")
-        NOTIFY.screenshot_client.take_screenshot('modal')
-        NOTIFY.screenshot_client.remove_screenshot()
+        if not NOTIFY.disabled:
+            NOTIFY.send_message("Modal found in MLOL")
+            NOTIFY.screenshot_client.take_screenshot(page, 'modal')
+            NOTIFY.screenshot_client.remove_screenshot()
 
         # Retrieving modal dismissal button
         modal_dismissal_button = page.locator("//div[@class='modal-footer']/button[@data-dismiss='modal']")
