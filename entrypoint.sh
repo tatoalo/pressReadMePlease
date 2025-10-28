ab5632c8c4c13ad0b470be210efbdbc209b4e23e
tatoalo <apogliaghi@gmail.com>
2025-10-28 13:41:11 +0000

    feat: added E2E startup script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# Run e2e startup test
+echo "Running E2E startup test..."
+python /src/e2e_startup.py
+
+# Check if e2e test passed
+if [ $? -ne 0 ]; then
+    echo "E2E startup test failed! Container will still start, but please check the logs."
+fi
+
+# Start cron
 cron -f
